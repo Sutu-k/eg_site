bab73d8e5f6b7f4ca0005f681a389c090fcf6a20
Souad Touré <soutou42@gmail.com>
2025-04-25 17:13:13 +0200

    feat: Ajout d'un bouton "Accéder" pour les projets avec URL, améliorant l'accès aux réalisations.

--- appli/resources/js/Pages/Landing.tsx
+++ appli/resources/js/Pages/Landing.tsx
@@ -1122,15 +1122,33 @@ export default function Landing() {
 
                                     <p className="text-white/80 text-sm mb-6 line-clamp-2 transform group-hover:translate-y-0 transition-transform duration-300">{project.subtitle}</p>
 
-                                    {/* Bouton "Voir plus" qui apparaît au survol */}
-                                    <div className="opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-y-2 group-hover:translate-y-0">
+                                    {/* Boutons qui apparaissent au survol */}
+                                    <div className="opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-y-2 group-hover:translate-y-0 flex space-x-4">
+                                        {/* Bouton "Voir détails" */}
                                         <button className="text-white text-sm font-medium flex items-center">
                                             Voir détails
                                             <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-                                    </svg>
+                                            </svg>
                                         </button>
-                                </div>
+
+                                        {/* Lien vers la réalisation si disponible */}
+                                        {project.url && (
+                                            <a
+                                                href={project.url}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="text-eg-tertiary text-sm font-medium flex items-center"
+                                                onClick={(e) => e.stopPropagation()}
+                                            >
+                                                Accéder
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                                </svg>
+                                            </a>
+                                        )}
+                                    </div>
                             </div>
 
                                 {/* Bouton d'info */}
