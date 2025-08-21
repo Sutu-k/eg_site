4536cc4367944b458e0dda43be0a2feee03b922e
VincentProPro <69010638+VincentProPro@users.noreply.github.com>
2025-08-21 20:16:33 +0000

    Update footer contact

--- appli/resources/js/Layouts/EGLayout.tsx
+++ appli/resources/js/Layouts/EGLayout.tsx
@@ -209,13 +209,13 @@ const EGLayout: React.FC<EGLayoutProps> = ({ title, children }) => {
                                         <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                                     </svg>
-                                    <span className="ml-2 text-sm text-white/80">contact@eg-conseil.com</span>
+                                    <span className="ml-2 text-sm text-white/80">oumar.camara@efficienceglobale.com</span>
                                 </li>
                                 <li className="flex items-start">
                                     <svg className="h-5 w-5 text-eg-secondary mt-0.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                         <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                                     </svg>
-                                    <span className="ml-2 text-sm text-white/80">+224 XX XX XX XX</span>
+                                    <span className="ml-2 text-sm text-white/80">+224 625 35 77 27</span>
                                 </li>
                             </ul>
                         </div>
